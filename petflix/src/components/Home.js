79fa9cf3e43d5d5c64a05bf35bcb80a02587fd84
WILIAN WAGNER <wilian_wagner@estudante.sc.senai.br>
2024-08-05 21:35:01 -0300

    feat: criação do  componente de exibição do video da tela de home

---
+++ petflix/src/components/Home.js
@@ -0,0 +1,21 @@
+// src/components/Home.js
+import React from 'react';
+import VideoCard from './VideoCard';
+
+const Home = () => {
+  // Simula uma lista de vídeos
+  const videos = [
+    { id: 1, title: 'Video 1', src: '/assets/videos/video1.mp4' },
+    { id: 2, title: 'Video 2', src: '/assets/videos/video2.mp4' },
+  ];
+
+  return (
+    <div>
+      {videos.map(video => (
+        <VideoCard key={video.id} video={video} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
