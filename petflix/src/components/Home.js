52ebc69425929548233b4b5bdecdcd36252faebd
WILIAN WAGNER <wilian_wagner@estudante.sc.senai.br>
2024-08-05 21:48:28 -0300

    Ajuste do local dos videos

--- petflix/src/components/Home.js
+++ petflix/src/components/Home.js
@@ -5,8 +5,8 @@ import VideoCard from './VideoCard';
 const Home = () => {
   // Simula uma lista de vídeos
   const videos = [
-    { id: 1, title: 'Video 1', src: '/assets/videos/video1.mp4' },
-    { id: 2, title: 'Video 2', src: '/assets/videos/video2.mp4' },
+    { id: 1, title: 'Video 1', src: '/assets/video1.mp4' },
+    { id: 2, title: 'Video 2', src: '/assets/video2.mp4' },
   ];
 
   return (
