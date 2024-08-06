d81958a115bf09be4a8a3bad5e792780a74fdccf
WILIAN WAGNER <wilian_wagner@estudante.sc.senai.br>
2024-08-05 21:29:14 -0300

    feat: Criando as páginas das rotas definidas

---
+++ petflix/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+// src/pages/HomePage.js
+import React from 'react';
+import Home from '../components/Home';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>PetFlix</h1>
+      <Home />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ petflix/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+// src/pages/LoginPage.js
+import React from 'react';
+import Login from '../components/Login';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
